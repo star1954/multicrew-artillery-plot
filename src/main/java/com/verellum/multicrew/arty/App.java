99a0fa34199e5c874e4cd9a3348b5bb3cb9d4581
Verellum <andidiwang@yahoo.com>
2024-08-13 03:58:32 -0400

    testing setup

--- src/main/java/com/verellum/multicrew/arty/App.java
+++ src/main/java/com/verellum/multicrew/arty/App.java
@@ -1,5 +1,10 @@
 package com.verellum.multicrew.arty;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
 import nu.pattern.OpenCV;
 
 /**
@@ -8,12 +13,17 @@ import nu.pattern.OpenCV;
  */
 public class App 
 {
-    public static void main( String[] args )
+
+    public static void main( String[] args ) throws URISyntaxException, FileNotFoundException
     {
         OpenCV.loadLocally();
+        CV_Interface cvo = new CV_Interface();
+        File file = new File(App.class.getResource("maps/test.txt").toURI());
+        Scanner sc = new Scanner(file);
         
-
+        System.out.println(sc.nextLine());
+        sc.close();
     }
-    CV_Interface cvo = new CV_Interface();
+    
     
 }
