49864eea0f070c236670c3cf1cb54ead500e41d9
Verellum <andidiwang@yahoo.com>
2024-08-13 02:33:11 -0400

    Initial commit

---
+++ src/main/java/com/verellum/multicrew/arty/App.java
@@ -0,0 +1,14 @@
+package com.verellum.multicrew.arty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
