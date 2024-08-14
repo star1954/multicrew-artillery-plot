408df4767423eff458126f540c20b2ea46f5aeff
Verellum <andidiwang@yahoo.com>
2024-08-14 05:57:07 -0400

    Created a class to handle program initialization(needs implementation)

---
+++ src/main/java/com/verellum/multicrew/arty/Init.java
@@ -0,0 +1,10 @@
+package com.verellum.multicrew.arty;
+
+public class Init {
+    public static void Initialize(){
+        extractResources();
+    }
+    private static void extractResources(){
+        //TODO extract resources and place into the temp directory
+    }
+}
