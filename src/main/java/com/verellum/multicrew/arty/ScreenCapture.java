fad699585128ed561abff6428d3e067d387f3b4b
Verellum <andidiwang@yahoo.com>
2024-08-14 05:58:33 -0400

    Added an overload for output Changed output to temp directory Added javadocs

--- src/main/java/com/verellum/multicrew/arty/ScreenCapture.java
+++ src/main/java/com/verellum/multicrew/arty/ScreenCapture.java
@@ -13,29 +13,53 @@ import javax.imageio.ImageIO;
  * Utility class to take screenshots
  */
 public class ScreenCapture {
-    
+
     private Robot robot;
 
     ScreenCapture() {
         try {
-			robot = new Robot();
-		} catch (AWTException e) {
-			e.printStackTrace();
-		}
+            robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * @return BufferedImage The screen captured
+     *         This captures the main monitor
+     */
     public BufferedImage capture() {
         Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
         return robot.createScreenCapture(screenRect);
     }
 
+    /**
+     * @param bi The BufferedImage to write to storage
+     *           Writes the image to the storage as output.jpg
+     */
     public void output(BufferedImage bi) {
-        File output = new File("output.jpg");
+        File output = new File("temp/output.jpg");
+        output.mkdirs(); //Create the temp directory if it isn't there already
         try {
-			ImageIO.write(bi, "jpg", output);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+            ImageIO.write(bi, "jpg", output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param bi       The BufferedImage to write to storage
+     * @param fileName The name of the file to be written
+     *                 Writes the image to the storage as a .jpg file
+     */
+    public void output(BufferedImage bi, String fileName) {
+        File output = new File("temp/".concat(fileName));
+        output.mkdirs(); //Create the temp directory if it isn't there already
+        try {
+            ImageIO.write(bi, "jpg", output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
